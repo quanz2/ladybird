92cfec70d25403a7508456dbcba3d6edd4c88407
missu1609 <85271234+quanz2@users.noreply.github.com>
2024-06-28 15:42:26 +0700

    Update ladybird.sh
    check AFK

--- Meta/ladybird.sh
+++ Meta/ladybird.sh
@@ -134,7 +134,24 @@ ensure_toolchain() {
     if [ "$(cmake -P "$LADYBIRD_SOURCE_DIR"/Meta/CMake/cmake-version.cmake)" -ne 1 ]; then
         build_cmake
     fi
-
+{
+    "name": "Ladybird Development",
+    "id": "ladybird",
+    "version": "2.0.0",
+    "description": "Enable development of Ladybird libraries and applications",
+    "options": {
+        "llvm_version": {
+            "type": "string",
+            "proposals": [
+                17,
+                18,
+                "trunk"
+            ],
+            "default": 18,
+            "description": "Select LLVM compiler version to use"
+        }
+    }
+}
     build_vcpkg
 }
 
@@ -171,7 +188,43 @@ build_and_run_lagom_target() {
     if [ -z "$lagom_target" ]; then
         lagom_target="ladybird"
     fi
+/*
+ * Copyright (c) 2020, the SerenityOS developers.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
 
+#pragma once
+
+#include <AK/Concepts.h>
+#include <AK/Find.h>
+#include <AK/Iterator.h>
+
+namespace AK {
+
+template<typename TEndIterator, IteratorPairWith<TEndIterator> TIterator>
+[[nodiscard]] constexpr bool all_of(
+    TIterator const& begin,
+    TEndIterator const& end,
+    auto const& predicate)
+{
+    constexpr auto negated_predicate = [](auto const& pred) {
+        return [&](auto const& elem) { return !pred(elem); };
+    };
+    return !(find_if(begin, end, negated_predicate(predicate)) != end);
+}
+
+template<IterableContainer Container>
+[[nodiscard]] constexpr bool all_of(Container&& container, auto const& predicate)
+{
+    return all_of(container.begin(), container.end(), predicate);
+}
+
+}
+
+#if USING_AK_GLOBALLY
+using AK::all_of;
+#endif
     build_target "${lagom_target}"
 
     if [ "$lagom_target" = "ladybird" ] && [ "$(uname -s)" = "Darwin" ]; then
